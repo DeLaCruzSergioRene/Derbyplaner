6bee3165328446654a7ca68509ce6f0916eaba18
Benjamin67634 <23308060610675@cetis61.edu.mx>
2026-05-25 09:57:51 -0500

    vistas

---
+++ views/Juego.py
@@ -0,0 +1,20 @@
+import flet as ft
+
+def juego(page: ft.Page, al_exito):
+    page.navigation_bar = ft.NavigationBar(
+        destinations=[
+            ft.NavigationBarDestination(icon=ft.Icons.EXPLORE, label="the"),
+            ft.NavigationBarDestination(icon=ft.Icons.COMMUTE, label="the"),
+            ft.NavigationBarDestination(
+                icon=ft.Icons.BOOKMARK_BORDER,
+                selected_icon=ft.Icons.BOOKMARK,
+                label="Favorites",
+            ),
+        ]
+    )
+
+    page.add(
+        ft.SafeArea(
+            content=ft.Text("Vacio, no hay nada!"),
+        )
+    )
--- views/Menu.py
+++ views/Menu.py
@@ -1,9 +1,27 @@
 import flet as ft
+from views.Juego import juego 
 
 def menu(page: ft.Page, al_exito):
-    page.add(ft.Container(
-            content=ft.Column([
-                ft.Text("eeeeeeeeeeeeeeeeeeeeee", size=16, weight="bold"),
-            ], spacing=15, horizontal_alignment="center"),
-            padding=30, expand=True, alignment=ft.alignment.Alignment(0, 0),
-        ))
+    page.navigation_bar = ft.NavigationBar(
+        destinations=[
+            ft.NavigationBarDestination(icon=ft.Icons.EXPLORE, label="Explore"),
+            ft.NavigationBarDestination(icon=ft.Icons.COMMUTE, label="Commute"),
+            ft.NavigationBarDestination(
+                icon=ft.Icons.BOOKMARK_BORDER,
+                selected_icon=ft.Icons.BOOKMARK,
+                label="Favorites",
+            ),
+        ]
+    )
+
+    page.add(
+        ft.SafeArea(
+            content=ft.Text("Body!"),
+        )
+    )
+    #page.add(ft.Container(
+    #        content=ft.Column([
+    #            ft.Text("eeeeeeeeeeeeeeeeeeeeee", size=16, weight="bold"),
+    #        ], spacing=15, horizontal_alignment="center"),
+    #        padding=30, expand=True, alignment=ft.alignment.Alignment(0, 0),
+    #    ))
