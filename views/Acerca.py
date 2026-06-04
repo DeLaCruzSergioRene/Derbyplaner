d27c81ada64351e031d5ef969422b3a99d3dc6b2
DeLaCruzSergioRene <23308060610508@cetis61.edu.mx>
2026-06-04 11:16:09 -0500

    texto

--- views/Acerca.py
+++ views/Acerca.py
@@ -4,18 +4,17 @@ def acercaDe(page: ft.Page):
     
     return ft.Container(    
         content=ft.Column([
-            ft.Text("ACERCA DE"),
-            ft.Text("Un pequeño juego inspirado en Uma Musume"),
-            ft.Text("Este juego desarrollado en flet con python es muy simple, solo crea selecciona un personaje, sus estadisticas, habilidades, etc y le das a guardar y luego ya a jugar"),
-            ft.Text("AVISO DE COPYRIGHT"),
-            ft.Text("esto un projecto escolar solo con fines personales y educativos"),
-            ft.Text("Uma Musume es una marca registrada de Cygames"),
-            ft.Text("El resto de recursos como imagenes los derechos van a sus respectivos creadores"),
-            ft.Image(src="assets\\img\\biw.jpg", width=700, height=700, fit="contain"),
+            ft.Text("ACERCA DE:"),
+            ft.Text("Un pequeño juego inspirado en Umamusume: Pretty Derby."),
+            ft.Text("Este juego desarrollado en flet con python es muy simple, seleccionas a la Umamusume que mas te agrade, seleccionas sus estadisticas con las barras de abajo, su terreno preferido y las habilidades que podría usar, creas una carrera con distancia y terreno que prefieras y listo, a jugar."),
+            ft.Text("(AVISO DE COPYRIGHT)"),
+            ft.Text("Esto un projecto escolar solo con fines personales y educativos."),
+            ft.Text("Umamusume es una marca registrada de Cygames."),
+            ft.Text("El resto de recursos como imagenes y cualquier asset van con los derechos y creditos a sus respectivos creadores."),
+            ft.Image(src="assets\\img\\biw.jpg", width=400, height=500, fit="contain"),
         ], horizontal_alignment="center", spacing=12),
         padding=20,
         alignment=ft.alignment.Alignment(0, 0),
         expand=True,
         bgcolor="#e4f4d0"
     )
-    
